b7547a1e6490f6e53c5e4bdcbb7a7f458dbbe183
YazanAljundy <yazanaljundy21@gmail.com>
2025-07-17 10:08:03 -0700

    create notes list view

---
+++ lib/views/widgets/notes_list_view.dart
@@ -0,0 +1,20 @@
+import 'package:flutter/material.dart';
+import 'package:tone_app/views/widgets/note_item.dart';
+
+class NotesListView extends StatelessWidget {
+  const NotesListView({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Expanded(
+      child: ListView.builder(
+        itemCount: 10,
+        itemBuilder:
+            (context, index) => Padding(
+              padding: const EdgeInsets.symmetric(vertical: 8),
+              child: NoteItem(),
+            ),
+      ),
+    );
+  }
+}
--- lib/views/widgets/notes_view_body.dart
+++ lib/views/widgets/notes_view_body.dart
@@ -1,6 +1,8 @@
 import 'package:flutter/material.dart';
 import 'package:font_awesome_flutter/font_awesome_flutter.dart';
 import 'package:tone_app/views/widgets/custom_app_bar.dart';
+import 'package:tone_app/views/widgets/note_item.dart';
+import 'package:tone_app/views/widgets/notes_list_view.dart';
 
 class NotesViewBody extends StatelessWidget {
   const NotesViewBody({super.key});
@@ -10,57 +12,7 @@ class NotesViewBody extends StatelessWidget {
     return Padding(
       padding: EdgeInsets.symmetric(horizontal: 24),
       child: Column(
-        children: const [SizedBox(height: 50), CustomAppBar(), NoteItem()],
-      ),
-    );
-  }
-}
-
-class NoteItem extends StatelessWidget {
-  const NoteItem({super.key});
-
-  @override
-  Widget build(BuildContext context) {
-    return Container(
-      padding: const EdgeInsets.only(top: 16, bottom: 16, left: 16),
-      decoration: BoxDecoration(
-        color: const Color(0xffFFCC80),
-        borderRadius: BorderRadius.circular(16),
-      ),
-      child: Column(
-        crossAxisAlignment: CrossAxisAlignment.end,
-        children: [
-          ListTile(
-            title: Text(
-              'Flutter Tips',
-              style: TextStyle(color: Colors.black, fontSize: 20),
-            ),
-            subtitle: Padding(
-              padding: const EdgeInsets.only(top: 16),
-              child: Text(
-                'build your career with yazan',
-                style: TextStyle(
-                  color: Colors.black.withOpacity(0.5),
-                  fontSize: 16,
-                ),
-              ),
-            ),
-            trailing: IconButton(
-              onPressed: () {},
-              icon: Icon(FontAwesomeIcons.trash, color: Colors.black, size: 22),
-            ),
-          ),
-          Padding(
-            padding: const EdgeInsets.only(right: 34),
-            child: Text(
-              'May 21 ,21',
-              style: TextStyle(
-                color: Colors.black.withOpacity(.4),
-                fontSize: 12,
-              ),
-            ),
-          ),
-        ],
+        children: const [SizedBox(height: 50), CustomAppBar(), NotesListView()],
       ),
     );
   }
