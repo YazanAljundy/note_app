74f7e6756b556cd11804954b232d9dd939093665
YazanAljundy <yazanaljundy21@gmail.com>
2025-07-17 09:50:29 -0700

    create note item

--- lib/views/widgets/notes_view_body.dart
+++ lib/views/widgets/notes_view_body.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:font_awesome_flutter/font_awesome_flutter.dart';
 import 'package:tone_app/views/widgets/custom_app_bar.dart';
 
 class NotesViewBody extends StatelessWidget {
@@ -8,7 +9,59 @@ class NotesViewBody extends StatelessWidget {
   Widget build(BuildContext context) {
     return Padding(
       padding: EdgeInsets.symmetric(horizontal: 24),
-      child: Column(children: const [SizedBox(height: 50), CustomAppBar()]),
+      child: Column(
+        children: const [SizedBox(height: 50), CustomAppBar(), NoteItem()],
+      ),
+    );
+  }
+}
+
+class NoteItem extends StatelessWidget {
+  const NoteItem({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      padding: const EdgeInsets.only(top: 16, bottom: 16, left: 16),
+      decoration: BoxDecoration(
+        color: const Color(0xffFFCC80),
+        borderRadius: BorderRadius.circular(16),
+      ),
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.end,
+        children: [
+          ListTile(
+            title: Text(
+              'Flutter Tips',
+              style: TextStyle(color: Colors.black, fontSize: 20),
+            ),
+            subtitle: Padding(
+              padding: const EdgeInsets.only(top: 16),
+              child: Text(
+                'build your career with yazan',
+                style: TextStyle(
+                  color: Colors.black.withOpacity(0.5),
+                  fontSize: 16,
+                ),
+              ),
+            ),
+            trailing: IconButton(
+              onPressed: () {},
+              icon: Icon(FontAwesomeIcons.trash, color: Colors.black, size: 22),
+            ),
+          ),
+          Padding(
+            padding: const EdgeInsets.only(right: 34),
+            child: Text(
+              'May 21 ,21',
+              style: TextStyle(
+                color: Colors.black.withOpacity(.4),
+                fontSize: 12,
+              ),
+            ),
+          ),
+        ],
+      ),
     );
   }
 }
