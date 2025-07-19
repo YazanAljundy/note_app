ef050c7acf6773237c53f02d71b4e6795e942801
YazanAljundy <yazanaljundy21@gmail.com>
2025-07-19 01:44:53 -0700

    add notes states

--- lib/add_notes_cubits/add_notes_cubit.dart
+++ lib/add_notes_cubits/add_notes_cubit.dart
@@ -2,6 +2,6 @@ import 'package:flutter/widgets.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 part 'add_notes_state.dart';
 
-class NotesCubit extends Cubit<NotesState> {
-  NotesCubit() : super(NotesInitial as NotesState);
+class AddNotesCubit extends Cubit<AddNotesState> {
+  AddNotesCubit() : super(AddNotesInitial as AddNotesState);
 }
--- lib/add_notes_cubits/add_notes_state.dart
+++ lib/add_notes_cubits/add_notes_state.dart
@@ -1,6 +1,16 @@
 part of 'add_notes_cubit.dart';
 
 @immutable
-abstract class NotesState {}
+abstract class AddNotesState {}
 
-class NotesInitial extends NotesState {}
+class AddNotesInitial extends AddNotesState {}
+
+class AddNotesLoading extends AddNotesState {}
+
+class AddNotesSaccess extends AddNotesState {}
+
+class AddNotesFailure extends AddNotesState {
+  final String errMessage;
+
+  AddNotesFailure(this.errMessage);
+}
