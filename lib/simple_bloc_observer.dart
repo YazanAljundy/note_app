12bcb5a5a9e98d0f79c7c6546326ba16779ed657
YazanAljundy <yazanaljundy21@gmail.com>
2025-07-19 22:44:33 -0700

    refactor multi bloc provider

--- lib/simple_bloc_observer.dart
+++ lib/simple_bloc_observer.dart
@@ -1,29 +1,39 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 
-class SimpleBlocObserver extends BlocObserver {
+class SimpleBlocObserver implements BlocObserver {
+  @override
+  void onCreate(BlocBase bloc) {
+    // TODO: implement onCreate
+
+    debugPrint('create=$bloc');
+  }
+
+  @override
+  void onClose(BlocBase bloc) {
+    // TODO: implement onClose
+
+    debugPrint('close=$bloc');
+  }
+
   @override
   void onEvent(Bloc bloc, Object? event) {
-    super.onEvent(bloc, event);
     // TODO: implement onEvent
   }
 
   @override
   void onError(BlocBase bloc, Object error, StackTrace stackTrace) {
     // TODO: implement onError
-    super.onError(bloc, error, stackTrace);
   }
 
   @override
   void onChange(BlocBase bloc, Change change) {
-    super.onChange(bloc, change);
     // TODO: implement onChange
     debugPrint('change=$change');
   }
 
   @override
   void onTransition(Bloc bloc, Transition transition) {
-    super.onTransition(bloc, transition);
     // TODO: implement onChange
   }
 }
--- lib/views/widgets/add_note_bottom_sheet.dart
+++ lib/views/widgets/add_note_bottom_sheet.dart
@@ -9,21 +9,24 @@ class AddNoteBottomSeet extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Padding(
-      padding: EdgeInsets.symmetric(horizontal: 16),
-      child: BlocConsumer<AddNotesCubit, AddNotesState>(
-        listener: (context, state) {
-          if (state is AddNotesFailure) {}
-          if (state is AddNotesSaccess) {
-            Navigator.pop(context);
-          }
-        },
-        builder: (context, state) {
-          return ModalProgressHUD(
-            inAsyncCall: state is AddNotesLoading ? true : false,
-            child: SingleChildScrollView(child: const AddNoteForm()),
-          );
-        },
+    return BlocProvider(
+      create: (context) => AddNotesCubit(),
+      child: Padding(
+        padding: EdgeInsets.symmetric(horizontal: 16),
+        child: BlocConsumer<AddNotesCubit, AddNotesState>(
+          listener: (context, state) {
+            if (state is AddNotesFailure) {}
+            if (state is AddNotesSaccess) {
+              Navigator.pop(context);
+            }
+          },
+          builder: (context, state) {
+            return ModalProgressHUD(
+              inAsyncCall: state is AddNotesLoading ? true : false,
+              child: SingleChildScrollView(child: const AddNoteForm()),
+            );
+          },
+        ),
       ),
     );
   }
